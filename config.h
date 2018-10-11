0e06840032b1c0b3c662e681ed79fad967a6d8b7
Emiel Kollof <emiel@prolocation.net>
2018-10-11 12:09:39 +0200

    Add monitor/kbd brightness keys. Requires 'light'. wq!

--- config.h
+++ config.h
@@ -15,6 +15,10 @@
 #define XF86AudioLowerVolume	0x1008ff11
 #define XF86AudioRaiseVolume	0x1008ff13
 #define XF86TouchpadToggle		0x1008ffa9
+#define	XF86MonBrightnessUp	0x1008ff02
+#define	XF86MonBrightnessDown	0x1008ff03
+#define XF86KbdBrightnessUp	0x1008ff05
+#define XF86KbdBrightnessDown	0x1008ff06
 
 /* appearance */
 /*static const char *fonts[] = {
@@ -38,16 +42,16 @@ static const char col_cyan[]        = "#005577";
 
 static const unsigned int borderpx 			= 1;	/* border pixel of windows */
 static const unsigned int snap 				= 10;	/* snap pixel */
-static const unsigned int tagpadding 		= 13;	
-static const unsigned int tagspacing 		= 5;	
+static const unsigned int tagpadding 			= 13;	
+static const unsigned int tagspacing 			= 5;	
 static const unsigned int gappx				= 12;	/* gap pixel between windows */
-static const unsigned int systraypinning 	= 0;	/* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing 	= 2;	/* systray spacing */
-static const int systraypinningfailfirst 	= 1;	/* 1: if pinning fails, display systray on the first monitor, 0: display systray on the last monitor*/
+static const unsigned int systraypinning 		= 0;	/* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing 		= 2;	/* systray spacing */
+static const int systraypinningfailfirst 		= 1;	/* 1: if pinning fails, display systray on the first monitor, 0: display systray on the last monitor*/
 static const int showsystray 				= 1;	/* 0 means no systray */
-static const int showbar 					= 1;	/* 0 means no bar */
-static const int topbar 					= 1;	/* 0 means bottom bar */
-static const int extrabar                    = 1;   /* 0 means no extra bar */
+static const int showbar 				= 1;	/* 0 means no bar */
+static const int topbar 				= 1;	/* 0 means bottom bar */
+static const int extrabar                    		= 1;   /* 0 means no extra bar */
 
 #define NUMCOLORS 9
 static const char colors[NUMCOLORS][MAXCOLORS][9] = {
@@ -96,12 +100,12 @@ static const Rule rules[] = {
 	 */
 	/* class			instance	title		tags mask	isfloating		monitor */
 	{ "Gimp",			NULL,		NULL,		0,				0,			-1 },
-	{ "Firefox",		NULL,		NULL,		1 << 0,			0,			-1 },
-	{ "Steam",			NULL,		NULL,		1 << 4,			1,			-1 },
-	{ "Nitrogen",		NULL,		NULL,		0,			  	1,			-1 },
-	{ "Lxappearance",	NULL,		NULL,		0,				1,			-1 },
+	{ "Firefox",			NULL,		NULL,		1 << 0,				0,			-1 },
+	{ "Steam",			NULL,		NULL,		1 << 4,				1,			-1 },
+	{ "Nitrogen",			NULL,		NULL,		0,			  	1,			-1 },
+	{ "Lxappearance",		NULL,		NULL,		0,				1,			-1 },
 	{ "XCalc",			NULL,		NULL,		0,				1,			-1 },
-	{ "Xgrabcolor",		NULL,		NULL,		0,				1,			-1 },
+	{ "Xgrabcolor",			NULL,		NULL,		0,				1,			-1 },
 };
 
 /* layout(s) */
@@ -130,64 +134,68 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] 			= "0"; /* component of dmenucmd, manipulated in spawn() */
-// static const char *dmenucmd[] 		= { "spawn_rofi", NULL };
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static char dmenumon[2] 		= "0"; /* component of dmenucmd, manipulated in spawn() */
+// static const char *dmenucmd[] 	= { "spawn_rofi", NULL };
+static const char *dmenucmd[] 		= { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 
 static const char *termcmd[] 		= { "st", NULL };
-static const char *volup[] 			= { "pulseaudio-ctl", "up", NULL };
+static const char *volup[] 		= { "pulseaudio-ctl", "up", NULL };
 static const char *voldown[] 		= { "pulseaudio-ctl", "down", NULL };
 static const char *voltoggle[] 		= { "pulseaudio-ctl", "mute", NULL };
 static const char *togtouchpad[] 	= { "touchpad-toggle.sh", NULL };
 static const char *screenshot[] 	= { "spectacle", NULL};
-static const char *passcmd[]        = { "passdmenu", NULL };
-static const char *lockcmd[]        = { "slock", NULL };
-
+static const char *passcmd[]        	= { "passdmenu", NULL };
+static const char *lockcmd[]      	= { "slock", NULL };
+static const char *monbrightup[]    	= { "light", "-A", "10", NULL };
+static const char *monbrightdown[]    	= { "light", "-U", "10", NULL };
+static const char *kbdbrightup[]    	= { "light", "-k", "-A", "10", NULL };
+static const char *kbdbrightdown[]    	= { "light", "-k", "-U", "10", NULL };
+static const char *kbdbrightoff[]    	= { "light", "-k", "-S", "0", NULL };
 
 static Key keys[] = {
 	/* modifier						key							function			argument */
 	{ MODKEY,						XK_p,						spawn,				{.v = dmenucmd } },
-	{ MODKEY|ShiftMask,				XK_p,						spawn,				{.v = passcmd } },
-	{ MODKEY|ControlMask,			XK_l,						spawn,				{.v = lockcmd } },
+	{ MODKEY|ShiftMask,					XK_p,						spawn,				{.v = passcmd } },
+	{ MODKEY|ControlMask,					XK_l,						spawn,				{.v = lockcmd } },
 	{ MODKEY,						XK_Return,					spawn,				{.v = termcmd } },
-	{ MODKEY|ShiftMask,				XK_b,						togglebar,			{0} },
-    { MODKEY|ControlMask|ShiftMask, XK_b,                       toggleextrabar,     {0} },
+	{ MODKEY|ShiftMask,					XK_b,						togglebar,			{0} },
+    	{ MODKEY|ControlMask|ShiftMask, 			XK_b,                       			toggleextrabar,     		{0} },
 	{ MODKEY,						XK_Right,					focusstack,			{.i = +1 } },
 	{ MODKEY,						XK_Left,					focusstack,			{.i = -1 } },
 	{ MODKEY,						XK_i,						incnmaster,			{.i = +1 } },
 	{ MODKEY,						XK_d,						incnmaster,			{.i = -1 } },
-	{ MODKEY|ShiftMask,				XK_Left,					setmfact,			{.f = -0.05} },
-	{ MODKEY|ShiftMask,				XK_Right,					setmfact,			{.f = +0.05} },
-	{ MODKEY|ShiftMask,				XK_Up,						setcfact,			{.f = +0.25} },
-	{ MODKEY|ShiftMask,				XK_Down,					setcfact,			{.f = -0.25} },
-	{ MODKEY|ShiftMask,				XK_o,						setcfact,			{.f =  0.00} },
+	{ MODKEY|ShiftMask,					XK_Left,					setmfact,			{.f = -0.05} },
+	{ MODKEY|ShiftMask,					XK_Right,					setmfact,			{.f = +0.05} },
+	{ MODKEY|ShiftMask,					XK_Up,						setcfact,			{.f = +0.25} },
+	{ MODKEY|ShiftMask,					XK_Down,					setcfact,			{.f = -0.25} },
+	{ MODKEY|ShiftMask,					XK_o,						setcfact,			{.f =  0.00} },
 	{ MODKEY,						XK_Down,					pushdown,			{0} },
-    { MODKEY,						XK_Up,						pushup,				{0} },
-	{ MODKEY|ShiftMask,				XK_Return,					zoom,				{0} },
+    { MODKEY,							XK_Up,						pushup,				{0} },
+	{ MODKEY|ShiftMask,					XK_Return,					zoom,				{0} },
 	{ MODKEY,						XK_Tab,						view,				{0} },
-	{ MODKEY|ShiftMask,				XK_x,						killclient,			{0} },
+	{ MODKEY|ShiftMask,					XK_x,						killclient,			{0} },
 	{ MODKEY,						XK_t,						setlayout,			{.v = &layouts[0]} },
 	{ MODKEY,						XK_f,						setlayout,			{.v = &layouts[1]} },
 	{ MODKEY,						XK_m,						setlayout,			{.v = &layouts[2]} },
 	{ MODKEY,						XK_b,						setlayout,			{.v = &layouts[3]} },
 	{ MODKEY,						XK_g,						setlayout,			{.v = &layouts[4]} },
 	{ MODKEY,						XK_space,					setlayout,			{0} },
-    { MODKEY,						XK_u,						togglefullscreen,	{0} },
-	{ MODKEY|ShiftMask,				XK_space,					togglefloating,		{0} },
+    { MODKEY,							XK_u,						togglefullscreen,		{0} },
+	{ MODKEY|ShiftMask,					XK_space,					togglefloating,			{0} },
 	{ MODKEY,						XK_0,						view,				{.ui = ~0 } },
-	{ MODKEY|ShiftMask,				XK_0,						tag,				{.ui = ~0 } },
+	{ MODKEY|ShiftMask,					XK_0,						tag,				{.ui = ~0 } },
 	{ MODKEY,						XK_comma,					focusmon,			{.i = -1 } },
 	{ MODKEY,						XK_period,					focusmon,			{.i = +1 } },
-	{ MODKEY|ShiftMask,				XK_comma,					tagmon,				{.i = -1 } },
-	{ MODKEY|ShiftMask,				XK_period,					tagmon,				{.i = +1 } },
+	{ MODKEY|ShiftMask,					XK_comma,					tagmon,				{.i = -1 } },
+	{ MODKEY|ShiftMask,					XK_period,					tagmon,				{.i = +1 } },
 	{ Mod4Mask,						XK_Up,						moveresize,			{.v = "0x -25y 0w 0h"} },
 	{ Mod4Mask,						XK_Down,					moveresize,			{.v = "0x 25y 0w 0h"} },
 	{ Mod4Mask,						XK_Left,					moveresize,			{.v = "-25x 0y 0w 0h"} },
 	{ Mod4Mask,						XK_Right,					moveresize,			{.v = "25x 0y 0w 0h"} },
-	{ Mod4Mask|ShiftMask,			XK_Up,						moveresize,			{.v = "0x 0y 0w -25h"} },
-	{ Mod4Mask|ShiftMask,			XK_Down,					moveresize,			{.v = "0x 0y 0w 25h"} },
-	{ Mod4Mask|ShiftMask,			XK_Left,					moveresize,			{.v = "0x 0y -25w 0h"} },
-	{ Mod4Mask|ShiftMask,			XK_Right,					moveresize,			{.v = "0x 0y 25w 0h"} },
+	{ Mod4Mask|ShiftMask,					XK_Up,						moveresize,			{.v = "0x 0y 0w -25h"} },
+	{ Mod4Mask|ShiftMask,					XK_Down,					moveresize,			{.v = "0x 0y 0w 25h"} },
+	{ Mod4Mask|ShiftMask,					XK_Left,					moveresize,			{.v = "0x 0y -25w 0h"} },
+	{ Mod4Mask|ShiftMask,					XK_Right,					moveresize,			{.v = "0x 0y 25w 0h"} },
 	TAGKEYS(						XK_1,						0)
 	TAGKEYS(						XK_2,						1)
 	TAGKEYS(						XK_3,						2)
@@ -197,13 +205,18 @@ static Key keys[] = {
 	TAGKEYS(						XK_7,						6)
 	TAGKEYS(						XK_8,						7)
 	TAGKEYS(						XK_9,						8)
-	{ MODKEY|ShiftMask,				XK_q,						quit,				{0} },
-	{ MODKEY|ShiftMask,				XK_r,						self_restart,		{0} },
-    { 0,							XK_Print,					spawn,				{.v = screenshot } },
-	{ 0,							XF86AudioRaiseVolume,		spawn,				{.v = volup } },
-    { 0,							XF86AudioLowerVolume,		spawn,				{.v = voldown } },
-    { 0,							XF86AudioMute,				spawn,				{.v = voltoggle } },
-    { 0,							XF86TouchpadToggle,			spawn,				{.v = togtouchpad } },
+	{ MODKEY|ShiftMask,					XK_q,						quit,				{0} },
+	{ MODKEY|ShiftMask,					XK_r,						self_restart,			{0} },
+    	{ 0,							XK_Print,					spawn,				{.v = screenshot } },
+	{ 0,							XF86AudioRaiseVolume,		spawn,						{.v = volup } },
+    	{ 0,							XF86AudioLowerVolume,		spawn,						{.v = voldown } },
+    	{ 0,							XF86AudioMute,				spawn,					{.v = voltoggle } },
+    	{ 0,							XF86TouchpadToggle,			spawn,					{.v = togtouchpad } },
+    	{ 0,							XF86MonBrightnessUp,			spawn,					{.v = monbrightup } },
+    	{ 0,							XF86MonBrightnessDown,			spawn,					{.v = monbrightdown } },
+    	{ 0,							XF86KbdBrightnessUp,			spawn,					{.v = kbdbrightup } },
+    	{ 0,							XF86KbdBrightnessDown,			spawn,					{.v = kbdbrightdown } },
+    	{ ShiftMask,						XF86KbdBrightnessDown,			spawn,					{.v = kbdbrightoff } },
 };
 
 /* button definitions */
