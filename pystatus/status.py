d16bbd8848bbfe8cde4f26306cc6bd981fb4c221
Emiel Kollof <emiel.kollof@gmail.com>
2018-11-29 21:30:32 +0100

    Added some nitfy portable things

--- pystatus/status.py
+++ pystatus/status.py
@@ -4,6 +4,10 @@ import os
 import sys
 import time
 import datetime
+import socket
+import psutil
+import ssl
+import urllib.request
 
 from Xlib.display import Display
 
@@ -12,12 +16,18 @@ def main():
     display = Display()
     root = display.screen().root
 
-    while(True):
+    # Static stuff
+    hostname = socket.gethostname()
+    localip = socket.gethostbyname(hostname)
+    pubip = urllib.request.urlopen('https://enabledns.com/ip',context=ssl._create_unverified_context()).read().decode("utf-8")
 
+    while(True):
+        cpuload = psutil.cpu_percent()
         now = datetime.datetime.now()
         curtime = now.strftime("%d-%m-%Y %H:%M:%S")
 
-        status = f"{curtime}"
+        status = f"CPU: {cpuload: >5}% - {curtime};" + \
+            f"Host: {hostname: <20} Local IP: {localip: <18} Public IP: {pubip: <18}"
         root.set_wm_name(status)
         display.sync()
         time.sleep(1)
